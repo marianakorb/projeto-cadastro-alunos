9ace1b3ae7e7bbac2cd1fc0dedb64019a43f9c74
mariana <mariana.korb.mk@gmail.com>
2023-10-03 18:05:03 -0300

    definindo a lógica

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/**
+ 
+cadastraAluno() 
+se todos os campos estiverem preenchidos:
+- pega as informacoes do formulario;
+- cria um objeto com essas informacões + um id para esse aluno;
+- esse objeto é adicionado na lista de alunos
+- direciona para a pagina listaAlunos após o cadastro
+senao:
+-exibe mensagem de erro;
+
+criar uma tabela com as informações do aluno
+*/
+
+/**
+Na página listaAlunos possui a opção de alterar o aluno.
+Quando for solicitado a alteração do aluno é direcionado para a pagina alterarAluno.
+- é preciso pegar as informações do aluno e imprimir nos respectivos inputs;
+- quando confirmar a alteração é atualizado o objeto que tiver o id do aluno;
+- exibe as informações atualizadas na lista de alunos;
+- para excluir um aluno será pego o objeto com o id do aluno e removido.
+*/
